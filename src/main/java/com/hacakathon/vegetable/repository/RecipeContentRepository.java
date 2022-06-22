339ab2c7330c3d062ad79916f52153d60e351f5f
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-06-23 03:10:24 +0900

    VoteService 생성

--- src/main/java/com/hacakathon/vegetable/repository/RecipeContentRepository.java
+++ src/main/java/com/hacakathon/vegetable/repository/RecipeContentRepository.java
@@ -1,6 +1,8 @@
 package com.hacakathon.vegetable.repository;
 
+import com.hacakathon.vegetable.domain.EMD;
 import com.hacakathon.vegetable.domain.RecepiContent;
+import com.hacakathon.vegetable.domain.VegeContent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +11,7 @@ import java.util.List;
 
 public interface RecipeContentRepository extends JpaRepository<RecepiContent, Long> {
 
-    boolean existsByRecepiContentId(long recepiContentId);
+    List<RecepiContent> findByEmd(EMD emd, Pageable pageable);
 
     RecepiContent findByRecepiContentId(long recepiContentId);
 
