0bb003ede1202d70ae5bca6d5cada260cc3efaf8
soso-p <parksohee113@gmail.com>
2022-06-22 19:58:12 +0900

    초기 세팅

---
+++ src/main/java/com/hacakathon/vegetable/VegeValleyApplication.java
@@ -0,0 +1,13 @@
+package com.hacakathon.vegetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegeValleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegeValleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacakathon/vegetable/VegeValleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacakathon.vegetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegeValleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
